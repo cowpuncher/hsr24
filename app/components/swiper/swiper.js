433e0ca486d3ca8f451664bb66b8de50fa32550e
Kirill Gorchakov <none@none>
2019-08-12 10:21:12 +0300

    fixed slider on main page
    --HG--
    branch : task_52920

--- app/components/swiper/swiper.js
+++ app/components/swiper/swiper.js
@@ -2,10 +2,16 @@ var swiper = new Swiper('.swiper-container', {
     observer: true,
     observeParents: true,
     slidesPerView: 4,
+    spaceBetween: 26,
     scrollbar: {
         el: '.swiper-scrollbar',
         hide: false,
         draggable: true
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
 
     // Responsive breakpoints
@@ -13,12 +19,24 @@ var swiper = new Swiper('.swiper-container', {
     // when window width is <= 480px
     480: {
       slidesPerView: 2,
+      spaceBetween: 10
+    },
+    767: {
+      spaceBetween: 10
     },
     // when window width is <= 640px
     1279: {
       slidesPerView: 3,
+      spaceBetween: 26
     }
   }
 });
 
+
+
+// $(window).resize(function() {
+//   $(".swiper-container").each(function(){
+//     this.swiper.update();
+//   });
+// });
 
