5ad95c500d782ce78a771bc030ea71c94fae7c07
Grigorev Dmitriy <solo-jumpstyle@mail.ru>
2020-02-05 15:22:01 +0300

    новая страница - десктоп
    --HG--
    branch : task_60652

--- app/components/swiper/swiper.js
+++ app/components/swiper/swiper.js
@@ -105,3 +105,87 @@ var swiperCategory  = new Swiper('[data-swiper-category-img]', {
   },
   // el: '.swiper-scrollbar'
 });
+
+
+var swiper = new Swiper('[data-tabs-slider-item-three]', {
+  observer: true,
+  observeParents: true,
+  slidesPerView: 3,
+  spaceBetween: 26,
+  allowTouchMove: false,
+  scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+      draggable: true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+  // when window width is <= 480px
+  534: {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    allowTouchMove: true
+  },
+  // when window width is <= 767px
+  767: {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    allowTouchMove: true
+  },
+  // when window width is <= 1279px
+  1279: {
+    slidesPerView: 3,
+    spaceBetween: 26
+  }
+}
+});
+
+var swiper = new Swiper('[data-slider-brands-min-item]', {
+  observer: true,
+  observeParents: true,
+  slidesPerView: 4,
+  spaceBetween: 0,
+  allowTouchMove: false,
+  scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+      draggable: true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+  // when window width is <= 480px
+  620: {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    allowTouchMove: true
+  },
+  // when window width is <= 767px
+  767: {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    allowTouchMove: true
+  },
+  // when window width is <= 1023px
+  1023: {
+    slidesPerView: 4,
+    spaceBetween: 26
+  },
+  // when window width is <= 1279px
+  1279: {
+    slidesPerView: 4,
+    spaceBetween: 26
+  }
+}
+});
